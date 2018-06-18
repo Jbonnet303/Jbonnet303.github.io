2d32498fa47d518da1afcc03d90f957f313fde25
Jon Bonnet <jbonnet303@gmail.com>
2018-06-18 15:41:10 -0400

    adding event listeners

--- JS/app.js
+++ JS/app.js
@@ -7,3 +7,45 @@ const connect4 = new Connect4('#connect4')
 
 
 });
+
+
+class Connect4 {
+  constructor (selector) {
+    this.ROWS = 5;
+    this.COLS = 6;
+    this.selector = selector;
+    this.createGrid();
+    this.setupEventListeners();
+  }
+
+  createGrid() {
+    const $board = $(this.selector);
+    for (let row = 0; row < this.ROWS; row++) {
+    const $row = $('<div>')
+    .addClass('row');
+    for (let col = 0; col < this.COLS; col++) {
+    const $col = $('<div>')
+    .addClass('col empty');
+    $('.col.empty').attr('data-col', col)
+    $('.col.empty').attr('data-row', row);
+
+    // $('board').attr('data-col', col)
+    // $('board').attr('data-row', row);
+
+    $row.append($col);
+    }
+    $board.append($row);
+
+    }
+  }
+    setupEventListeners() {
+      const $board = $(this.selector);
+
+      $board.on('mouseenter', '.col.empty', function() {
+        //const col = $(this).data('col');
+        //console.log(col);
+
+      console.log('here', this);
+    })
+  }
+}
--- main.js/connect4.js
+++ main.js/connect4.js
@@ -1,27 +1,36 @@
-class Connect4 {
-  constructor (selector) {
-    this.ROWS = 5;
-    this.COLS = 6;
-    this.selector = selector;
-    this.createGrid();
-  }
 
-  createGrid() {
-    const $board = $(this.selector);
-    for (let row = 0; row < this.ROWS; row++) {
-    const $row = $('<div>')
-    .addClass('row');
-    for (let col = 0; col < this.COLS; col++) {
-    const $col = $('<div>')
-    .addClass('col empty');
-    $row.append($col);
-    }
-    $board.append($row);
-
-    }
-  }
-}
-
+// class Connect4 {
+//   constructor (selector) {
+//     this.ROWS = 5;
+//     this.COLS = 6;
+//     this.selector = selector;
+//     this.createGrid();
+//     this.setupEventListeners();
+//   }
+//
+//   createGrid() {
+//     const $board = $(this.selector);
+//     for (let row = 0; row < this.ROWS; row++) {
+//     const $row = $('<div>')
+//     .addClass('row');
+//     for (let col = 0; col < this.COLS; col++) {
+//     const $col = $('<div>')
+//     .addClass('col empty');
+//     $row.append($col);
+//     }
+//     $board.append($row);
+//
+//     }
+//   }
+//     setupEventListeners() {
+//
+//       const $board = $(this.selector);
+//
+//       $board.on('mouseenter', '.col.empty', function() {
+//       console.log('here', this);
+//     })
+//   }
+// }
 
 
 
